6bb809efedd64ea79d44b6f910aca009a5842d32
eduardorobles22 <eduardorobles@oxoft.io>
2021-01-12 01:00:30 -0400

    added config yml to site

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
     );
 
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("admin");
 
     return {
         passthroughFileCopy: true,
